790e4c3079236f5397c6146e83056d41581d016a
luis-tapia12 <j.contreras.t@hotmail.com>
2024-10-01 22:37:49 -0600

    add Assertions messages

--- src/main/java/com/jcontrerast/utils/Assertions.java
+++ src/main/java/com/jcontrerast/utils/Assertions.java
@@ -3,16 +3,18 @@ package com.jcontrerast.utils;
 import com.jcontrerast.utils.exception.AssertionException;
 
 public class Assertions {
+    private final static String SUFFIX = "Validation failed: ";
+
     public static void isNotNull(Object object) {
         if (object == null)
-            throw new AssertionException();
+            throw new AssertionException(SUFFIX + Messages.ASSERTION_IS_NOT_NULL_ERROR);
     }
 
     public static void isPositive(Number number) {
         isNotNull(number);
 
         if (number.intValue() < 0)
-            throw new AssertionException();
+            throw new AssertionException(SUFFIX + Messages.ASSERTION_IS_POSITIVE_ERROR);
     }
 
     public static void isGreaterThan(Number numberA, Number numberB) {
@@ -20,7 +22,7 @@ public class Assertions {
         isNotNull(numberB);
 
         if (numberA.doubleValue() <= numberB.doubleValue())
-            throw new AssertionException();
+            throw new AssertionException(SUFFIX + Messages.ASSERTION_IS_GREATER_THAN_ERROR + numberB);
     }
 
     public static void isLessThan(Number numberA, Number numberB) {
@@ -28,7 +30,7 @@ public class Assertions {
         isNotNull(numberB);
 
         if (numberA.doubleValue() >= numberB.doubleValue())
-            throw new AssertionException();
+            throw new AssertionException(SUFFIX + Messages.ASSERTION_IS_LESS_THAN_ERROR + numberB);
     }
 
     public static void isGreaterOrEqualThan(Number numberA, Number numberB) {
@@ -36,7 +38,7 @@ public class Assertions {
         isNotNull(numberB);
 
         if (numberA.doubleValue() < numberB.doubleValue())
-            throw new AssertionException();
+            throw new AssertionException(SUFFIX + Messages.ASSERTION_IS_GREATER_OR_EQUAL_THAN_ERROR + numberB);
     }
 
     public static void isLessOrEqualThan(Number numberA, Number numberB) {
@@ -44,6 +46,6 @@ public class Assertions {
         isNotNull(numberB);
 
         if (numberA.doubleValue() > numberB.doubleValue())
-            throw new AssertionException();
+            throw new AssertionException(SUFFIX + Messages.ASSERTION_IS_LESS_OR_EQUAL_THAN_ERROR + numberB);
     }
 }
--- src/main/java/com/jcontrerast/utils/exception/AssertionException.java
+++ src/main/java/com/jcontrerast/utils/exception/AssertionException.java
@@ -1,4 +1,12 @@
 package com.jcontrerast.utils.exception;
 
+import java.io.Serial;
+
 public class AssertionException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public AssertionException(String message) {
+        super(message);
+    }
 }
